73dbb6dc23f8da5b3bdef2b0af97886362257d20
Khoger Dosky <58479335+kdosky@users.noreply.github.com>
2020-03-08 22:22:16 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * The HelloWorld class implemements an application that prints
+ * "Hello World!" to the standard output.
+ */
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // Display the string.
+        System.out.println("This is my first coding project at UMGC!");
+        System.out.println("CMIS 141 is awesome!");
+    }
+}
